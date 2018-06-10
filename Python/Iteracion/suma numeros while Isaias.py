22645167871ab69ced14e37121b3af946ef92ca2
isotomurillo <36709641+isotomurillo@users.noreply.github.com>
2018-06-09 23:36:24 -0600

    Add files via upload

---
+++ Python/Iteracion/suma numeros while Isaias.py
@@ -0,0 +1,11 @@
+class suma ():
+    def __init__(self):
+        pass
+    def sumar (self):
+        num=0
+        numero=1
+        while numero > 0:
+            numero=int(input("Ingrese el numero "))
+            num += numero
+        print ("La Suma es : ",num)
+        
